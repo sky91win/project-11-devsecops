96615c5040bab7bb762560fd325f7a20fb62fc0b
sky91win <118115504+sky91win@users.noreply.github.com>
2025-12-20 23:32:45 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -2,20 +2,14 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.15.3"
 
-  cluster_name    = "project8-eks"
+  cluster_name    = "project9-eks"
   cluster_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  # 🔐 kubectl access from laptop
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
-
-  # ❌ Disable KMS & custom encryption
-  create_kms_key             = false
-  create_cloudwatch_log_group = false
-  cluster_encryption_config  = {}
 
   eks_managed_node_groups = {
     default = {
