c03d709eace9863e29ec3e9b8ed3618929e46e68
sky91win <118115504+sky91win@users.noreply.github.com>
2025-12-20 21:33:11 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -10,10 +10,10 @@ module "eks" {
 
   eks_managed_node_groups = {
     default = {
-      desired_size   = 2
+      desired_size   = 1
       min_size       = 1
-      max_size       = 3
-      instance_types = ["t3.medium"]
+      max_size       = 1
+      instance_types = ["t3.micro"]
     }
   }
 }
