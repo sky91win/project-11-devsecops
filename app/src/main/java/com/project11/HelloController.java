e8b1fed85f859a793899c97335a7c9c709042229
sky91win <118115504+sky91win@users.noreply.github.com>
2025-12-21 20:41:21 +0530

    Update HelloController.java

--- app/src/main/java/com/project11/HelloController.java
+++ app/src/main/java/com/project11/HelloController.java
@@ -2,6 +2,7 @@ package com.project11;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class HelloController {
@@ -14,5 +15,11 @@ public class HelloController {
     @GetMapping("/health")
     public String health() {
         return "OK";
+    }
+
+    // 🔗 Redirect to YouTube video after deploy
+    @GetMapping("/video")
+    public RedirectView openVideo() {
+        return new RedirectView("https://youtu.be/mh0WBGPs0ZQ?si=AU6RH4c_sTeDYrOZ");
     }
 }
